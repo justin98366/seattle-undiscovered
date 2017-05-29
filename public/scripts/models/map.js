51708fed1e5a50bc5a97669ea4a00b29db7d9df6
timothy mitchell <timothy.p.mitchell92@gmail.com>
2017-05-29 14:21:45 -0700

    added filter categorys

--- public/scripts/models/map.js
+++ public/scripts/models/map.js
@@ -3,10 +3,7 @@
 var gmarkers = [];
 
 var map;
-var infowindow = new google.maps.InfoWindow({
-    content: ''
-});
-var service;
+var infowindow;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 47.60616, lng: -122.328111},
@@ -19,29 +16,50 @@ function initMap() {
 }
 
 function addMarker(marker) {
-    var category = marker[4];
-    var title = marker[1];
-    var pos = new google.maps.LatLng(marker[2], marker[3]);
-    var content = marker[1];
-    var description = marker[5];
+  var category = marker[4];
+  var title = marker[1];
+  var pos = new google.maps.LatLng(marker[2], marker[3]);
+  var content = marker[1];
+  var description = marker[5];
 
-    marker = new google.maps.Marker({
-        title: title,
-        position: pos,
-        category: category,
-        map: map,
-        description: description
-    });
-    gmarkers.push(markers);
+  var marker = new google.maps.Marker({
+    title: title,
+    position: pos,
+    category: category,
+    map: map,
+    description: description
+  });
+  gmarkers.push(marker);
 
-        // Marker click listener
-        google.maps.event.addListener(markers, 'click', (function (markers, content) {
-            return function () {
-                console.log('Gmarker 1 gets pushed');
-                infowindow.setContent(content);
-                infowindow.open(map, markers);
-                map.panTo(this.getPosition());
-                map.setZoom(15);
-            }
-        })(markers, content));
+  // Marker click listener
+  // google.maps.event.addListener(markers, 'click', (function (markers, content) {
+  //     return function () {
+  //         console.log('Gmarker 1 gets pushed');
+  //         infowindow.setContent(content);
+  //         infowindow.open(map, markers);
+  //         map.panTo(this.getPosition());
+  //         map.setZoom(15);
+  //     }
+  // })(markers, content));
+  var infowindow = new google.maps.InfoWindow({
+   content: `${marker.title}
+    ${marker.description}`
+ });
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+
+}
+var filterMarkers = function (category) {
+  for (let i = 0; i < markers.length; i++) {
+    var marker = gmarkers[i];
+    // If is same category or category not picked
+    if (marker.category == category || category.length === 0) {
+      marker.setVisible(true);
     }
+    // Categories don't match
+    else {
+      marker.setVisible(false);
+    }
+  }
+}
