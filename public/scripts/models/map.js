54a1b53495ccb90e000d83944d8d67432b2e6444
timothy mitchell <timothy.p.mitchell92@gmail.com>
2017-05-29 11:23:17 -0700

    added google maps api

--- public/scripts/models/map.js
+++ public/scripts/models/map.js
@@ -1,1 +1,9 @@
+'use strict';
 
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 47.60616, lng: -122.328111},
+    zoom: 13
+  });
+}
