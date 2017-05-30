9e05000903fedaf3fed0f777e4e70df69d08ddab
Justin Green <justin98366@gmail.com>
2017-05-30 10:05:40 -0700

    added about contoller and page.js CDN

--- public/data/places-data.js
+++ public/data/places-data.js
@@ -1,6 +1,6 @@
 var markers = [
   [0, 'Columbian Tower', 47.604514, -122.330484, 'points-of-interest', 'This is the content'],
-  [1, 'Kells Irish Pub', 47.610594, -122.342332, 'restaurant', 'This is a bar'],
+  [1, 'Kells Irish Pub', 47.610594, -122.342332, 'restaurants', 'This is a bar'],
   [2, 'test', 47.610594, -122.342332, 'activities', 'ipsum'],
   [3, 'slkdjfsld', 47.610594, -122.342332, 'nightlife', 'sldkjfsl'],
   [4, 'bob', 47.610594, -122.342332, 'points-of-interest', 'bob']
