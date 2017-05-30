caef71797e1a6cfe1bc2d578ccb306da50c0a564
Justin Green <justin98366@gmail.com>
2017-05-30 12:00:08 -0700

    the api data is doing what we want

--- public/data/places-data.js
+++ public/data/places-data.js
@@ -1,5 +1,5 @@
 var markers = [
-  [0, 'Columbian Tower', 47.604514, -122.330484, 'points-of-interest', 'This is the content'],
+  [0, 'Columbian Tower', 47.604514, -122.330484, 'points-of-interest', 'This is the content', ],
   [1, 'Kells Irish Pub', 47.610594, -122.342332, 'restaurants', 'This is a bar'],
   [2, 'test', 47.610594, -122.342332, 'activities', 'ipsum'],
   [3, 'slkdjfsld', 47.610594, -122.342332, 'nightlife', 'sldkjfsl'],
